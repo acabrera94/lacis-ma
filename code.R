b4c991287e131ec6850f84eb86bf3ef1f845245f
acabrera94 <cabreramonsalve.a@gmail.com>
2023-07-11 16:46:03 -0400

    Social Class (not including education)

--- code.R
+++ code.R
@@ -15,7 +15,7 @@ pacman::p_load(tidyverse,
                foreign,
                coefplot,
                fastDummies,
-               psych)
+               knitr)
 
 rm(list=ls())       # borrar todos los objetos en el espacio de trabajo
 options(scipen=999) # valores sin notación científica
@@ -127,27 +127,31 @@ frq(db$credencial)
 
 #Construcción de variable "skills_2digit"
 db$skills_2digit <- car::recode(db$ISCO08,
-                                "1000:1120=11; 1200:1223=12; 1300:1349=13;
-                         1400:1439=14; 2100:2166=21; 2200:2269=22; 2300:2359=23;
-                         2400:2434=24; 2500:2529=25; 2600:2659=26; 3100:3155=31;
-                         3200:3259=32; 3300:3359=33; 3400:3435=34; 3500:3522=35;
-                         4100:4132=41; 4200:4229=42; 4300:4323=43; 4400:4419=44;
-                         5100:5169=51; 5200:5249=52; 5300:5329=53; 5400:5419=54;
-                         6100:6130=61; 6200:6224=62; 6300:6340=63; 7100:7133=71; 
-                         7200:7234=72; 7300:7323=73; 7400:7422=74; 7500:7549=75;
-                             8100:8189=81; 8200:8219=82; 8300:8350=83; 9100:9129=91;
-                             9200:9216=92; 9300:9334=93; 9400:9412=94; 9500:9520=95;
-                             9600:9629=96")
+                                "1000=10; 1100:1120=11; 1200:1223=12; 1300:1349=13;
+                                1400:1439=14; 2000=20; 2100:2166=21; 2200:2269=22;
+                                2300:2359=23; 2400:2434=24; 2500:2529=25; 2600:2659=26;
+                                3000=30; 3100:3155=31; 3200:3259=32; 3300:3359=33; 3400:3435=34;
+                                3500:3522=35; 4000=40; 4100:4132=41; 4200:4229=42; 4300:4323=43;
+                                4400:4419=44; 5000=50; 5100:5169=51; 5200:5249=52; 5300:5329=53;
+                                5400:5419=54; 6000=60; 6100:6130=61; 6200:6224=62; 6300:6340=63;
+                                7000=70; 7100:7133=71; 7200:7234=72; 7300:7323=73; 7400:7422=74;
+                                7500:7549=75; 8000=80; 8100:8189=81; 8200:8219=82; 8300:8350=83;
+                                9000=90; 9100:9129=91; 9200:9216=92; 9300:9334=93; 9400:9412=94;
+                                9500:9520=95; 9600:9629=96")
 
 frq(db$skills_2digit)
 
 
+#Construcción de variable Skills:
+db$skills <- car::recode(db$skills_2digit,
+                         recodes = "10:26=1; c(30,31,32,33,34,35,36,60,61,72)=2;
+                                    c(40,41,42,43,44,50,51,52,53,54,62,63,70,71,73,74,75,76,76,77,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96)=3")
+
+frq(db$skills)
 #Construcción de variable "skills_2digit"
-db$skills_1digit <- car::recode(db$ISCO08,
-                                "1000:1439=1; 2000:2659=2; 3000:3522=3; 4000:4419=4; 5000:5419=5;
-                                6000:6340=6; 7000:7549=7; 8000:8350=8; 9000:9629=9")
 
 frq(db$skills_1digit)
+
 
 
 
@@ -157,6 +161,10 @@ db$num_empleados <- car::recode(db$NEMPLOY,
                                 "1:9=1; 10:6000=2; 0=3")
 
 frq(db$num_empleados)
+
+
+
+
 
 
 #Construcción de Clase Social
@@ -172,22 +180,22 @@ db$css[db$auto_emp == 1 & db$num_empleados == 1 & db$WRKSUP == 1] <- 2
 db$css[db$auto_emp == 1 & db$num_empleados == 3] <- 3
 
 # Condition 4: Directivo/Supervisor Experto (BA) (Value: 4) - (Skills grupo 10 al 26 (isco) + Correción por credencial)
-db$css[db$auto_emp == 2 & db$WRKSUP == 1 & db$skills_1digit %in% c(1, 2) & db$credencial == 3 ] <- 4
+db$css[db$auto_emp == 2 & db$WRKSUP == 1 & db$skills == 1 & db$credencial == 3 ] <- 4
 
 # Condition 5: Experto (BA+) No directivo (Value: 5)
-db$css[db$auto_emp == 2 & db$WRKSUP == 2 & db$skills_1digit %in% c(1, 2) & db$credencial == 3] <- 5
+db$css[db$auto_emp == 2 & db$WRKSUP == 2 & db$skills == 1 & db$credencial == 3] <- 5
 
 # Condition 6: Directivo/Supervisor Semi-Credencializado (Value: 6)
-db$css[db$auto_emp == 2 & db$WRKSUP == 1 & db$skills_1digit %in% c(3, 6)] <- 6
+db$css[db$auto_emp == 2 & db$WRKSUP == 1 & db$skills == 2] <- 6
 
 # Condition 7: Obrero semi-credencializado (Value: 7)
-db$css[db$auto_emp == 2 & db$WRKSUP == 2 & db$skills_1digit %in% c(3, 6)] <- 7
+db$css[db$auto_emp == 2 & db$WRKSUP == 2 & db$skills == 2] <- 7
 
 # Condition 8: Directivo/Supervisor no credencializado (Value: 8)
-db$css[db$auto_emp == 2 & db$WRKSUP == 1 & db$skills_1digit %in% c(7, 9)] <- 8
+db$css[db$auto_emp == 2 & db$WRKSUP == 1 & db$skills == 3] <- 8
 
 # Condition 9: Proletariado tradicional (Value: 9)
-db$css[db$auto_emp == 2 & db$WRKSUP == 2 & db$skills_1digit %in% c(7, 9)] <- 9
+db$css[db$auto_emp == 2 & db$WRKSUP == 2 & db$skills == 3] <- 9
 
 frq(db$css)
 
@@ -274,11 +282,7 @@ db2<-select(db,
 #### Summary descriptive tables ####
 
 ## 1. Descriptives
-
-stats <- psych::describe(db2)
-
-
-markdown_table <- psych::describeToMarkdown(stats)  
+plot_frq(db2$css)
   
 ########### 3.1 Multivariate Analysis #############
 
@@ -291,8 +295,6 @@ db$css<-as_factor(db$css) #Clase Social Marx
 db$sex<-as_factor(db$SEX) #Sexo
 db$cs_sub2<-as_factor(db$cs_sub) #Clase Social Subj.
 db$pos_pol2<-as_factor(db$pos_pol) #Posición política
-
-
 
 
 
