ecab65e53a40cc5656176b78bd6ff8cf517ba881
acabrera94 <cabreramonsalve.a@gmail.com>
2023-07-23 14:00:04 -0400

    Models, export data set protocol.

--- code.R
+++ code.R
@@ -52,9 +52,10 @@ db<-select(db,
            V24, #Political: Express views on the internet
            V25, #Political: Using media to get political news
            V27, #Member: Political Party
-           V28, #Trade Union
+           UNION, #Trade Union
            V48, #Political Self-Placement
-           ISCO08 #Occupation (ISCO 2008)
+           ISCO08, #Occupation (ISCO 2008)
+           PARTLIV # Living in steady partnership
 )
 
 
@@ -87,9 +88,10 @@ recoding_specs <- list(
   V24 = c(8, 9),
   V25 = c(8, 9),
   V27 = c(8, 9),
-  V28 = c(8, 9),
+  UNION = c(8, 9),
   V48 = c(98, 99),
-  ISCO08= c(0, 9998, 9999, 0110, 0210, 0310))
+  ISCO08= c(0, 9998, 9999, 0110, 0210, 0310),
+  PARTLIV= c(7, 9, 0))
 
 for (var in names(recoding_specs)) {
   if (!is.null(recoding_specs[[var]])) {
@@ -164,6 +166,9 @@ db$num_empleados <- car::recode(db$NEMPLOY,
 frq(db$num_empleados)
 
 
+#Construcción de participa de unions o 
+db$unionized <- car::recode(db$UNION,
+                                "1:2=1; 3:4=0")
 
 
 
@@ -202,18 +207,44 @@ frq(db$css)
 
 
 
+
 ## 6.2 Construcción de variables de control
+
+
+
 #Clase Social subjetiva
 db$cs_sub <- car::recode(db$TOPBOT,
-                         "1:4=1; 5:6=2; 7:10=3")
+                         "1:4=2; 5:6=1; 7:10=0") # 2= WK-Class / 2=Middle Groups / 0=Upper Class
 
 frq(db$cs_sub) 
+
+
+
+## Construcción variable working class else = wk_else
+db$wk_else <- car::recode(db$css,
+                          "1:6=0; 7:9=1") #0 = Not working class / 1= Working class 
+
+## Construcción variable: correctly class identity (correct_ci)
+db$correct_ci <- NA
+
+# Con comando ifelse -> 1 = correct_class_identity / 0 = incorrect_class_identity
+db$correct_ci<- ifelse(db$wk_else == 1 & db$cs_sub == 2, 1,
+                       ifelse(db$wk_else == 1 & db$cs_sub %in% c(0, 1), 0, NA))
+
 
 #Posición Política
 db$pos_pol <- car::recode(db$V48,
                           "0:4=1; 5=2; 6:10=3")
 
 frq(db$pos_pol) 
+
+
+
+# Situación marital
+db$partner <- car::recode(db$PARTLIV,
+                          "1:2=1; 3=0")
+
+
 
 # Construcción de variable acción colectiva (sumatoria)
 #Loop that recodes, 1:2 = 1 (Yes), 3:4 = 2 (No)
@@ -259,13 +290,29 @@ db$css <- set_labels(db$css,
                                "Proletario tradicional"=9)) #Clase Social
 
 db$cs_sub <- set_labels(db$cs_sub,
-                        labels=c("Clase Subjetiva baja"=1, "Clase Subjetiva Media"=2,
-                                 "Clase Subjetiva Alta"=3)) #Clase Social Subjetiva
+                        labels=c("Clase Subjetiva baja"=2, "Clase Subjetiva Media"=3,
+                                 "Clase Subjetiva Alta"=0)) #Clase Social Subjetiva
 
 
 db$pos_pol <- set_labels(db$pos_pol,
                          labels=c("Izquierda"=1, "Centro"=2,
                                   "Derecha"=3)) #Posición Política
+
+
+db$correct_ci <- set_labels(db$correct_ci,
+                         labels=c("Corresponding Class Identity"=1,
+                                  "Not corresponding Class Identity" = 0)) #Correct Class identity
+
+db$unionized <- set_labels(db$unionized,
+                            labels=c("Have participated in unions"=1,
+                                     "Haven't participated in unions"=0)) #Unionizedorrect Class identityC
+
+db$partner <- set_labels(db$partner,
+                           labels=c("In partnership"=1,
+                                    "Not in partnership"=0)) #Partnership
+
+
+
 
 #Seleccionamos otra bdd
 db2<-select(db,
@@ -275,6 +322,27 @@ db2<-select(db,
             pos_pol,
             css,
             cs_sub)
+
+
+
+# Export Data base
+
+# Set the folder path
+folder_path <- "C:/Users/Alvaro C/Dropbox/3. Educacion/1. MA_LACIS/Lacis_MA/base"
+
+# Create the folder if it doesn't exist
+if (!dir.exists(folder_path)) {
+  dir.create(folder_path, recursive = TRUE)
+}
+
+# Replace 'your_data' with the name of your database object
+save(db, file = paste0(folder_path, "/db.RData"))
+
+
+# Load the data from the RData file
+#load("C:/Users/Alvaro C/Dropbox/3. Educacion/1. MA_LACIS/Lacis_MA/base/db.RData")
+
+
 
 
 ############## PARTE 4 #############
@@ -305,26 +373,75 @@ db$pos_pol2<-as_factor(db$pos_pol) #Posición política
 
 # Modelo A
 reg1<-lm(acc ~ css, data = db)
-sjPlot::tab_model(list(reg1), show.ci=FALSE, p.style = "stars",
+sjPlot::tab_model(list(reg1), show.ci=FALSE, show.se = TRUE,
+                  show.aic = TRUE, show.r2 = TRUE, collapse.se = TRUE,
+                  title = "Predictores de XXX",
+                  p.style = "stars",
                   string.pred = "Predictores", string.est = "β",digits = 3,
                   dv.labels = c("Modelo 1"))
 
 # Modelo A.2
-reg2<-lm(acc ~ css+cs_sub2, data = db)
+reg2<-lm(acc ~ css+cs_sub2+sex, data = db)
 sjPlot::tab_model(list(reg1, reg2), show.ci=FALSE, p.style = "stars",
                   string.pred = "Predictores", string.est = "β",digits = 3,
                   dv.labels = c("Modelo 1", "Modelo 2"))
 
 # Modelo A.3
-reg3<-lm(acc ~ css+cs_sub2+pos_pol2, data = db)
+reg3<-lm(acc ~ css+cs_sub2+AGE, data = db)
 sjPlot::tab_model(list(reg1, reg2, reg3), show.ci=FALSE, p.style = "stars",
                   string.pred = "Predictores", string.est = "β",digits = 3,
                   dv.labels = c("Modelo 1", "Modelo 2", "Modelo 3"))
 # Modelo A.4
-reg4<-lm(acc ~ css+cs_sub2+pos_pol2+AGE+sex, data = db)
+reg4<-lm(acc ~ css+AGE+partner, data = db)
 sjPlot::tab_model(list(reg1, reg2, reg3, reg4), show.ci=FALSE, p.style = "stars",
                   string.pred = "Predictores", string.est = "β",digits = 3,
                   dv.labels = c("Modelo 1", "Modelo 2", "Modelo 3", "Modelo 4"))
+# Modelo A.5
+reg5<-lm(acc ~ css+cs_sub2+AGE+sex+partner, data = db)
+sjPlot::tab_model(list(reg1, reg2, reg3, reg4, reg5), show.ci=FALSE, show.se = TRUE,
+                  show.aic = TRUE, show.r2 = TRUE, collapse.se = TRUE,
+                  show.reflvl = TRUE,
+                  title = "Predictores de XXX",
+                  p.style = "stars",
+                  string.pred = "Predictores", string.est = "β",digits = 3,
+                  dv.labels = c("Modelo 1", "Modelo 2", "Modelo 3", "Modelo 4", "Modelo 5"))
+
+
+####
+
+# Modelo B.1
+mod1<-lm(acc ~ correct_ci, data = db)
+sjPlot::tab_model(list(mod1), show.ci=FALSE, p.style = "stars",
+                  string.pred = "Predictores", string.est = "β",digits = 3,
+                  dv.labels = c("Modelo 1"))
+
+# Modelo B.2
+mod2<-lm(acc ~ correct_ci+unionized, data = db)
+sjPlot::tab_model(list(mod1, mod2), show.ci=FALSE, p.style = "stars",
+                  string.pred = "Predictores", string.est = "β",digits = 3,
+                  dv.labels = c("Modelo 1", "Modelo 2"))
+
+# Modelo B.3
+mod3<-lm(acc ~ correct_ci+unionized+pos_pol2+AGE, data = db)
+sjPlot::tab_model(list(mod1, mod2, mod3), show.ci=FALSE, p.style = "stars",
+                  string.pred = "Predictores", string.est = "β",digits = 3,
+                  dv.labels = c("Modelo 1", "Modelo 2", "Modelo 3"))
+
+# Modelo B.4
+mod4<-lm(acc ~ correct_ci+unionized+pos_pol2+sex, data = db)
+sjPlot::tab_model(list(mod1, mod2, mod3, mod4), show.ci=FALSE, p.style = "stars",
+                  string.pred = "Predictores", string.est = "β",digits = 3,
+                  dv.labels = c("Modelo 1", "Modelo 2", "Modelo 3", "Modelo 4"))
+
+# Modelo B.5
+mod5<-lm(acc ~ correct_ci+unionized+pos_pol2+AGE+sex, data = db)
+sjPlot::tab_model(list(mod1, mod2, mod3, mod4, mod5), show.ci=FALSE, show.se = TRUE,
+                  show.aic = TRUE, show.r2 = TRUE, collapse.se = TRUE,
+                  title = "Predictores de XXX",
+                  p.style = "stars",
+                  string.pred = "Predictores", string.est = "β",digits = 3,
+                  dv.labels = c("Modelo 1", "Modelo 2", "Modelo 3", "Modelo 4", "Modelo 5"))
+
 
 
 #. Visualización
